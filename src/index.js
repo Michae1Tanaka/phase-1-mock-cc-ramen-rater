6b81339193a832120ae813631a4d6536302ce354
Michael Tanaka <tanakam4214@gmail.com>
2023-04-21 13:52:23 -0700

    Advanced Deliverables

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 // write your code here
 
-document.addEventListener("DOMContentLoaded", () => {
-  //After dom loads, users should see the div#ramen-menu images, request data from server to get all ramen Obj, then display image for each of the ramen using <img> inside div#ramen-menu
-  fetch("http://localhost:3000/ramens")
-    .then((resp) => resp.json())
-    .then((data) => data.forEach(createAndAttachRamenImages));
-});
 
-//GLobal Variables
+
+//Global Variables
 //get h2 tag with class .name for name of ramen
 const ramenName = document.querySelector(".name");
 
@@ -27,21 +22,57 @@ const ratingDisplay = document.getElementById("rating-display");
 const commentDisplay = document.getElementById("comment-display");
 
 //get form tag with id new-ramen for when user submits new ramen
-const form = document.getElementById("new-ramen");
+const createRamenForm = document.getElementById("new-ramen");
 
-function createAndAttachRamenImages(ramen, index) {
+//get edit form for user's own comments/rating
+const editRamenForm = document.getElementById("edit-ramen");
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  //After dom loads, users should see the div#ramen-menu images, request data from server to get all ramen Obj, then display image for each of the ramen using <img> inside div#ramen-menu
+  fetch("http://localhost:3000/ramens")
+    .then((resp) => resp.json())
+    .then((data) => data.forEach(createAndAttachRamenImages));
+
+  //After dom loads, first Ramen info appears in the spotlight area
+  fetch("http://localhost:3000/ramens")
+    .then((resp) => resp.json())
+    .then((data) => {
+      detailImage.src = data[0].image;
+      ramenName.textContent = data[0].name;
+      restaurant.textContent = data[0].restaurant;
+      ratingDisplay.textContent = data[0].rating;
+      commentDisplay.textContent = data[0].comment;
+    });
+});
+
+
+
+
+//creates a ramen image for each ramen inside db.json
+function createAndAttachRamenImages(ramen) {
   //create image tag for ramen photos
   const image = document.createElement("img");
-  
-  //set id and source for ramen images in the menu and append them there.
+
+  //set source for ramen images in the menu and append them there.
   image.setAttribute("src", ramen.image);
-  image.setAttribute("id", `image_${index + 1}`);
   ramenMenu.appendChild(image);
 
   //add event listener to each image. Event = 'click'
   image.addEventListener("click", imageEventHandler);
 
-  //create callback function for event handler
+  //add event listener to each image that deletes the ramen that is double clicked with mouse
+  image.addEventListener("dblclick", (e) => {
+    //creates an alert for user to confirm if user wants to delete a ramen from the menu
+    const confirmed = confirm("Are you sure you want to delete?");
+    if (confirmed) {
+      image.remove(e.target);
+    }
+  });
+
+  //If image is clicked its info from db.json fills the spotlit ramen's info
   function imageEventHandler(e) {
     //after event occurs set detailImage source as events target's source
     detailImage.src = ramen.image;
@@ -60,13 +91,47 @@ function createAndAttachRamenImages(ramen, index) {
   }
 }
 
+
+
+
+//Events
+
 //add event listener to the submit form to add new ramen image to the menu
-form.addEventListener("submit",newRamenEventHandler)
+createRamenForm.addEventListener("submit", newRamenEventHandler);
+
+//adds an event to the edit form that updates the rating and the comment of the chosen ramen.
+editRamenForm.addEventListener("submit",editRamenEventHandler)
+
+
+
+//Event Handlers
 
 //Event handler for adding new ramen image to the menu
-function newRamenEventHandler(e){
+function newRamenEventHandler(e) {
   e.preventDefault();
-  const newRamenImage = document.createElement('img');
-  newRamenImage.setAttribute('src', e.target[2].value)
-  ramenMenu.appendChild(newRamenImage)
+
+  //creates a new image tag for the new ramen
+  const newRamenImage = document.createElement("img");
+
+  //sets image source as the user submitted photo
+  newRamenImage.setAttribute("src", e.target[2].value);
+
+  //attaches new image to the menu
+  ramenMenu.appendChild(newRamenImage);
+
+  //resets form after submit
+  createRamenForm.reset();
+}
+
+function editRamenEventHandler(e){
+  e.preventDefault();
+
+  //updates ramen's rating with users input from the form
+  ratingDisplay.textContent = e.target[0].value;
+
+  //updates ramens comment with the users input from the form
+  commentDisplay.textContent = e.target[1].value;
+
+  //resets form after submit
+  editRamenForm.reset();
 }
